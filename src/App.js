88cda50070ad21f064368553fbcddddfc63a7d45
Nicholas Huynh <nickhuynhq@gmail.com>
2022-09-06 16:35:27 -0400

    added routes and dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Warehouses from './pages/Warehouses/Warehouses';
+import Inventory from "./pages/Inventory/Inventory"
+import AddEditWarehouse from './pages/AddEditWarehouse/AddEditWarehouse';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Warehouses />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/warehouses" element={<Warehouses />} />
+        <Route path="/warehouses/:warehouseId" element={<WarehouseDetails />} />
+        <Route path="/warehouses/:warehouseId/edit" element={<AddEditWarehouse />} />
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
---
+++ src/pages/AddEditWarehouse/AddEditWarehouse.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddEditWarehouse = () => {
+  return (
+    <div>AddEditWarehouse</div>
+  )
+}
+
+export default AddEditWarehouse
