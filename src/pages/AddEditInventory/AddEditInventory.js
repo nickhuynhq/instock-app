a6b2cf98c52c2b41c65817334fded15761701502
Nicholas Huynh <nickhuynhq@gmail.com>
2022-09-07 14:40:29 -0400

    Formatting changes

--- src/pages/AddEditInventory/AddEditInventory.js
+++ src/pages/AddEditInventory/AddEditInventory.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ArrowBack from '../../assets/icons/arrow_back-24px.svg'
+import "./AddEditInventory.scss"
 
 const AddEditInventory = () => {
 
@@ -12,45 +13,38 @@ const AddEditInventory = () => {
       <form className="add-edit-inventory__form">
 
         <div className='form__main'>
-          <div className="form-item-details">
+          <div className="form__item-details">
             <h2 className="form__title">Item Details</h2>
-            <label className="form__label" htmlFor="name">
-              Item Name
-              <input id="name" name="name" className="form__text-input"type="text"/>
-            </label>
-            <label className="form__label" htmlFor="description">
-              Description
-              <textarea id="description" name="description" className="form__text-area"></textarea>
-            </label>
-            <label className="form__label" htmlFor="category">
-              Category
-              <select id="category" name="category" className="form__select-input">
-                <option value="electronics">Electronics</option>
-              </select>
-            </label>
+            <label className="form__label" htmlFor="name">Item Name</label>
+            <input id="name" name="name" className="form__text-input"type="text"/>
+            <label className="form__label" htmlFor="description">Description</label>
+            <textarea id="description" name="description" className="form__text-area"></textarea>
+            <label className="form__label" htmlFor="category">Category</label>
+            <select id="category" name="category" className="form__select-input">
+              <option value="electronics">Electronics</option>
+            </select>
           </div>
-          <div className="form-item-availability">
+          <div className="form__item-availability">
             <h2 className="form__title">Item Availability</h2>
 
             <p className="form__label">Status</p>
-            <input type="radio" id="in-stock" name="status" value="in-stock"
-              checked />
-            <label for="in-stock">In stock</label>
-            <input type="radio" id="out-of-stock" name="status" value="out-of-stock"
-              checked />
-            <label for="out-of-stock">Out of stock</label>
+            <div>
+              <input type="radio" id="in-stock" name="status" value="in-stock"/>
+              <label htmlFor="in-stock">In stock</label>
+            </div>
 
-            <label className="form__label" htmlFor="quantity">
-              Quantity
-              <input id="quantity" name="quantity" className="form__number-input" type="number"/>
-            </label>
+            <div>
+              <input type="radio" id="out-of-stock" name="status" value="out-of-stock"/>
+              <label htmlFor="out-of-stock">Out of stock</label>
+            </div>
+            
+            <label className="form__label" htmlFor="quantity">Quantity</label>
+            <input id="quantity" name="quantity" className="form__number-input" type="number"/>
 
-            <label className="form__label" htmlFor="warehouse">
-              Warehouse
-              <select id="warehouse" name="warehouse" className="form__select-input">
-                <option value="">Please select</option>
-              </select>
-            </label>
+            <label className="form__label" htmlFor="warehouse">Warehouse</label>
+            <select id="warehouse" name="warehouse" className="form__select-input">
+              <option value="">Please select</option>
+            </select>
           </div>
         </div>
         
