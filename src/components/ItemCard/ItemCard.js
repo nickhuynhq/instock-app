df15f68d5ef633767789ecde4c929dafc8cc9f46
rowena fonseka <rafonseka@gmail.com>
2022-09-07 23:43:35 -0400

    Create inventory card to populate in inventory list component. Added get request to inventory list. Did some stylings but need to finish

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,29 +1,43 @@
-import "./ItemCard.scss"
-
-const ItemCard = ({id, name, address, city, country, contactName, contactEmail, contactPhone}) => {
-    return (
-        <div className="card">
-            <div className="card__info">
-                <div className="card__info--side">
-                    <span className="card__info--label">Warehouse</span>
-                    <span className="card__info--span">{name}</span>
-                    <span className="card__info--label">Address</span>
-                    <span className="card__info--span">{address}{city}{country}</span>
-                </div>
-                <div className="card__info--side">
-                    <span className="card__info--label">Contact Name</span>
-                    <span className="card__info--span">{contactName}</span>
-                    <span className="card__info--label">Contact Information</span>
-                    <span className="card__info--span">{contactPhone}</span>
-                    <span className="card__info--span">{contactEmail}</span>
-                </div>
-            </div>
-            <div className="icons">
-                <img alt="logo"></img>
-                <img alt="logo"></img>
-            </div>
+import "./ItemCard.scss";
+import Delete from "../../assets/icons/delete_outline-24px.svg";
+import edit from "../../assets/icons/edit-24px.svg";
+const ItemCard = ({
+  id,
+  name,
+  address,
+  city,
+  country,
+  contactName,
+  contactEmail,
+  contactPhone,
+}) => {
+  return (
+    <div className="card">
+      <div className="card__info">
+        <div className="card__info--side">
+          <span className="card__info--label">Warehouse</span>
+          <span className="card__info--span">{name}</span>
+          <span className="card__info--label">Address</span>
+          <span className="card__info--span">
+            {address}
+            {city}
+            {country}
+          </span>
         </div>
-    )
-}
+        <div className="card__info--side">
+          <span className="card__info--label">Contact Name</span>
+          <span className="card__info--span">{contactName}</span>
+          <span className="card__info--label">Contact Information</span>
+          <span className="card__info--span">{contactPhone}</span>
+          <span className="card__info--span">{contactEmail}</span>
+        </div>
+      </div>
+      <div className="icons">
+        <img src={Delete} alt="logo" />
+        <img src={edit} alt="logo" />
+      </div>
+    </div>
+  );
+};
 
-export default ItemCard
+export default ItemCard;
--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import InventoryCard from "../../components/InventoryCard/InventoryCard";
+import "./Inventory.scss";
 
 const Inventory = () => {
-  return (
-    <div>Inventory</div>
-  )
-}
+  const [inventories, setInventories] = useState(null);
 
-export default Inventory
+  useEffect(() => {
+    axios.get("http://localhost:8081/inventory").then((response) => {
+      setInventories(response.data);
+    });
+  }, []);
+
+  if (!inventories) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <main className="inventory">
+      <div className="inventory__header">
+        <h1 className="inventory__title">Inventory</h1>
+        <div className="inventory__wrap">
+          <input className="inventory__search" placeholder="Search..." />
+          <button className="inventory__add">+ Add New Item</button>
+        </div>
+      </div>
+      <div className="inventory-card">
+        {inventories.map((inventory) => {
+          return (
+            <InventoryCard
+              key={inventory.id}
+              id={inventory.id}
+              itemName={inventory.itemName}
+              category={inventory.category}
+              status={inventory.status}
+              quantity={inventory.quantity}
+              warehouseName={inventory.warehouseName}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default Inventory;
