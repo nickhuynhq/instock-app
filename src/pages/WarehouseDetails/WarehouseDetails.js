09721eb8ea6b2dd343da83f30e27c7e15b9128d1
rowena fonseka <rafonseka@gmail.com>
2022-09-08 21:18:15 -0400

    working on rendering inventory list for warehouse details

--- src/pages/WarehouseDetails/WarehouseDetails.js
+++ src/pages/WarehouseDetails/WarehouseDetails.js
@@ -1,35 +1,57 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import InventoryCard from "../../components/InventoryCard/InventoryCard";
+import axios from "axios";
 
 const WareHouseDetails = () => {
-  const {warehouseId} = useParams()
-  const [warehouse, setWarehouse] = useState("")
-  
-  useEffect (()=>{
-    axios.get(`http://localhost:8081/warehouse/${warehouseId}`)
-      .then((response)=>{
-        setWarehouse(response.data)
-      })
-  },[warehouseId])
+  const { warehouseId } = useParams();
+  const [warehouse, setWarehouse] = useState("");
+  const [details, setDetails] = useState("");
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8081/warehouse/${warehouseId}`)
+      .then((response) => {
+        setWarehouse(response.data);
+      });
+  }, [warehouseId]);
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:8081/").then((response) => {
+  //     setDetails(response.data);
+  //   });
+  // }, [warehouseId]);
 
   if (warehouse === "") {
-    return <h1>Loading . . .</h1>
+    return <h1>Loading . . .</h1>;
   }
-  
+
   return (
     <main className="details">
-      <section className='warehouse'>
-        <h1 className='warehouse__name'>{warehouse.name}</h1>
-        <div className='warehouse__details'>
-          <span className='warehouse__label'>Warehouse Address</span>
-          <span className='warehouse__address'>{warehouse.address}</span>
+      <section className="warehouse">
+        <h1 className="warehouse__name">{warehouse.name}</h1>
+        <div className="warehouse__details">
+          <span className="warehouse__label">Warehouse Address</span>
+          <span className="warehouse__address">{warehouse.address}</span>
         </div>
+        {/* <div className="inventory-list">
+          {details.map((detail) => {
+            return (
+              <InventoryCard
+                key={detail.id}
+                id={detail.id}
+                itemName={detail.itemName}
+                category={detail.category}
+                status={detail.status}
+                quantity={detail.quantity}
+              />
+            );
+          })}
+        </div> */}
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default WareHouseDetails
+export default WareHouseDetails;
